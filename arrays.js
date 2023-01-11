ad456be875c84752acfbc0b3d7c40d6dc66ff378
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-01-11 17:23:15 -0500

    Showcase different methods of calling on the arrays

--- arrays.js
+++ arrays.js
@@ -3,3 +3,16 @@ var cats = ["tabby", "Bangal", "Tiger", "calico"];
 var luckNumbers = [7, 555, 777, 22];
 
 var winterCurrently = [true, true, true, true]
+
+// The pop will take out the element in the position specified
+cats.pop([3]);
+console.log(cats);
+
+// This method will log only the element in the selected position 
+console.log(winterCurrently[3]);
+ // This method will add an element at the end of the array.
+luckNumbers.push(576);
+console.log(luckNumbers);
+// This method will shift all the elements to the left, the number of spots specified
+cats.shift(1);
+console.log(cats);
